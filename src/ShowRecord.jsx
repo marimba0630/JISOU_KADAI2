d198cda3e9a6e63208d090f1aad47552a6a1ea85
marimba0630 <rn.marimba@gmail.com>
2025-12-24 18:28:35 +0900

    src change test

--- src/ShowRecord.jsx
+++ src/ShowRecord.jsx
@@ -1,11 +1,11 @@
 export const ShowRecord = (props) => {
-    const {records, onClick} = props;
+    const { records, onClick } = props;
     return (
         <div>
-            {records.map((record) => ( 
-            <li key={record.id}>{record.title} {record.time}時間 <button onClick={() => onClick(record.id)}>削除</button></li> 
-            ))} 
+            {records.map((record) => (
+                <li key={record.id}>{record.title} {record.time}時間 <button onClick={() => onClick(record.id)}>Delete</button></li>
+            ))}
         </div>
     );
-    
+
 }
